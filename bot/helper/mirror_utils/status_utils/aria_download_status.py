248de1fcd4058712686b0d47fb09644fa9b712b2
NiTRONDC1 <84825812+NiTRONDC@users.noreply.github.com>
2021-07-16 20:34:59 +0530

    Update aria_download_status.py

--- bot/helper/mirror_utils/status_utils/aria_download_status.py
+++ bot/helper/mirror_utils/status_utils/aria_download_status.py
@@ -97,11 +97,11 @@ class AriaDownloadStatus(Status):
         LOGGER.info(f"Cancelling Download: {self.name()}")
         download = self.aria_download()
         if download.is_waiting:
-            self.__listener.onDownloadError("Cancelled by user")
+            self.__listener.onDownloadError("Download Cancelled By User 🥺❗️")
             aria2.remove([download], force=True)
             return
         if len(download.followed_by_ids) != 0:
             downloads = aria2.get_downloads(download.followed_by_ids)
             aria2.remove(downloads, force=True)
-        self.__listener.onDownloadError('Download stopped by user!')
+        self.__listener.onDownloadError('Download Cancelled By User 🥺❗️')
         aria2.remove([download], force=True)
